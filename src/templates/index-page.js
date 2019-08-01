e6fe2ae06c0e433d59dd87a16d44ee48794a52e5
nikita <nikita.seliverstov@tptlive.ee>
2019-08-01 14:37:31 +0300

    fixes + preview

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
-
+import 'scss/gatstrap.scss'
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
